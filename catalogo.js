39b27362e3c59a613512b8280f7f0008535637ca
LeywinMC <101605761+LeywinMC@users.noreply.github.com>
2022-06-01 23:46:02 -0500

    Delete catalogo.js

--- catalogo.js
+++
@@ -1,5 +0,0 @@
-const items = document.getElementsByTagName('option');
-items[1].style.background = 'black';
-items[1].style.color = 'yellow';
-items[0].style.background = 'black';
-items[0].style.color = 'yellow';
